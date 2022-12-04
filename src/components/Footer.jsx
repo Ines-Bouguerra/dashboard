33b81b8ecfcd096a4c70aeeb895903d2a31e4bdf
Ines Bouguerra <ines.bouguerra.22@gmail.com>
2022-12-04 21:20:36 +0100

    Fix Header && Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © 2022 All rights reserved by Ines Bouguerra -{' '}
+      <a href="https://www.linkedin.com/in/ines-bouguerra/" className="text-blue-500 hover:text-blue-600">Ines Bouguerra</a>
+    </p>
+  </div>
+);
+
+export default Footer;
