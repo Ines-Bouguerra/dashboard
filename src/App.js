2d484875a7fff4de05b7671d7e7fc3ca9e20e1de
Ines Bouguerra <ines.bouguerra.22@gmail.com>
2022-12-04 01:19:43 +0100

    Fix style

--- src/App.js
+++ src/App.js
@@ -28,17 +28,18 @@ const App = () => {
               </div>
             ) : (
                 <div className="w-0 dark:bg-secondary-dark-kbg">
-                  sidebar w-0
+                  <Sidebar /> 
                 </div>
             )
           }
           <div className={
-            `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? ' md:ml-72' : 'flex-2'}`
+            activeMenu
+              ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+              : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
           }>
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
             </div>
-          </div>
           <div>
             <Routes>
               \* dashboard *\
@@ -66,6 +67,7 @@ const App = () => {
               <Route path="/pyramid" element={<Pyramid />} />
               <Route path="/stacked" element={<Stacked />} />
             </Routes>
+            </div>
           </div>
         </div>
       </BrowserRouter>
